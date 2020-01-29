66c207b8bbd782bc50bf3d1f0587d419d191334d
Diego Vieira <dieegov@live.com>
2020-01-29 13:20:56 -0300

    initial files

---
+++ Startup.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using VueCliMiddleware;
+
+namespace aspnetcore_vuejs_template
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllersWithViews();
+
+            // In production, the React files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/build";
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+       public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+            } else {
+                app.UseExceptionHandler ("/Error");
+                app.UseHsts ();
+            }
+
+            app.UseHttpsRedirection ();
+            app.UseStaticFiles ();
+            app.UseSpaStaticFiles ();
+
+            app.UseRouting ();
+
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllers ();
+
+                endpoints.MapToVueCliProxy (
+                    "{*path}",
+                    new SpaOptions { SourcePath = "ClientApp" },
+                    npmScript: (System.Diagnostics.Debugger.IsAttached) ? "serve" : null,
+                    regex: "Compiled successfully",
+                    forceKill : true
+                );
+            });
+        }
+    }
+}
